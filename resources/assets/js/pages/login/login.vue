a04f4c562872746dff7abb61909fa3748c11b90d
Vitor Leonel <falecom@vitorleonel.com>
2018-07-14 00:22:34 -0300

    metodo submit

--- resources/assets/js/pages/login/login.vue
+++ resources/assets/js/pages/login/login.vue
@@ -1,17 +1,44 @@
 <template>
   <div class="login">
-    <form class="form">
-      <input type="text" class="input" placeholder="seu e-mail">
-      <input type="password" class="input" placeholder="sua senha">
+    <form class="form" @submit.prevent="submitForm">
+			<label for="email">E-mail</label>
+      <input type="text" class="input" id="email" autofocus placeholder="seu e-mail">
 
-      <button class="button">Entrar</button>
+			<label for="password">Senha</label>
+      <input type="password" class="input" id="password" placeholder="sua senha">
+
+      <button type="submit" class="button" :disabled="isLoading">
+				{{ isLoading ? 'Aguarde...' : 'Entrar' }}
+			</button>
     </form>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'Login',
+		name: 'Login',
+
+		data() {
+			return {
+				isLoading: false,
+			}
+		},
+
+		methods: {
+			submitForm() {
+				if(! this.preValidate()) {
+					return;
+				}
+
+				this.isLoading = true;
+
+				setTimeout(() => this.isLoading = false, 4000);
+			},
+
+			preValidate() {
+				return true;
+			},
+		},
   }
 </script>
 
