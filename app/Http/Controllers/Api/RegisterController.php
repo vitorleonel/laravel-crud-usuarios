d750f6c91af7452cf0c73a783dca1350f0488d1c
Vitor Leonel <falecom@vitorleonel.com>
2018-07-15 21:25:45 -0300

    cadastro de usuarios

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -2,10 +2,29 @@
 
 namespace App\Http\Controllers\Api;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\Register\RegisterRequest;
 use App\Http\Controllers\Controller;
+
+use App\User;
 
 class RegisterController extends Controller
 {
-    //
+	/**
+	 * Register a user.
+	 *
+	 * @param StoreRequest $request
+	 */
+	public function handler(RegisterRequest $request)
+	{
+		$data = $request->only(['name', 'email', 'password']);
+		$user = User::create($data);
+
+		if(! $user) {
+			return response()->json([
+				'message' => 'User not created. Try again!'
+			], 400);
+		}
+
+		return response()->json(null, 201);
+	}
 }
