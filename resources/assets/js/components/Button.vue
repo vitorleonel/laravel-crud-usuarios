596b4bc4f46d19337a13c308a4da7af94abcd34c
Vitor Leonel <falecom@vitorleonel.com>
2018-07-16 00:25:54 -0300

    componente de botao e modal

---
+++ resources/assets/js/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+	<button :type="type" class="bg-white border border-blue hover:bg-blue-dark text-blue hover:text-white p-3 rounded" :class="{'shadow': shadow}" v-if="outlined">{{ text }}</button>
+	<button :type="type" class="bg-blue hover:bg-blue-dark text-white p-3 rounded" :class="{'shadow': shadow}" v-else>{{ text }}</button>
+</template>
+
+<script>
+	export default {
+		props: {
+			text: {
+				required: true,
+				type: String
+			},
+			type: {
+				type: String,
+				default: 'button'
+			},
+			outlined: {
+				type: Boolean,
+				default: false
+			},
+			shadow: {
+				type: Boolean,
+				default: false
+			}
+		},
+	}
+</script>
+
---
+++ resources/assets/js/components/Modal.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="w-screen h-screen fixed pin flex justify-center items-center">
+		<div class="w-screen h-screen fixed pin bg-black opacity-50" @click="closeModal"></div>
+
+		<div class="w-2/5 bg-white absolute p-4 rounded fixed">
+			<slot></slot>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		methods: {
+			closeModal() {
+				this.$emit('closeModal');
+			},
+		},
+	}
+</script>
+
